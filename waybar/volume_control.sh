0a5899b850c43453cdf8fcdabd156f49bc3e83a2
chocopndn <chocopndn@gmail.com>
2025-04-19 03:28:58 +0800

    Update

--- waybar/volume_control.sh
+++ waybar/volume_control.sh
@@ -5,27 +5,25 @@ current_volume=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | tr -d
 
 case $1 in
     raise)
-        # Raise volume by 5%
+        # Increase volume by 5%
         pactl set-sink-volume @DEFAULT_SINK@ +5%
-        # Ensure the volume doesn't exceed 100%
+        # Limit maximum volume to 100%
         if [ "$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | tr -d '%')" -gt 100 ]; then
             pactl set-sink-volume @DEFAULT_SINK@ 100%
         fi
         ;;
     lower)
-        # Lower volume by 5%
+        # Decrease volume by 5%
         pactl set-sink-volume @DEFAULT_SINK@ -5%
-        # Ensure the volume doesn't go below 0%
+        # Prevent volume from going below 0%
         if [ "$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | tr -d '%')" -lt 0 ]; then
             pactl set-sink-volume @DEFAULT_SINK@ 0%
         fi
         ;;
-    mute)
-        # Toggle mute
-        pactl set-sink-mute @DEFAULT_SINK@ toggle
-        ;;
     *)
-        echo "Usage: $0 {raise|lower|mute}"
+        # Display usage instructions
+        echo "Usage: $0 {raise|lower}"
         exit 1
         ;;
 esac
+
